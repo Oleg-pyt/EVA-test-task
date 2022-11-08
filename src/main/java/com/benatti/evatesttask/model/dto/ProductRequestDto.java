04875b73153d0ed57cf58009ec8c63ce93a38103
Oleh Sunytsia <ssunica0@gmail.com>
2022-11-08 11:25:09 +0200

    products dto

---
+++ src/main/java/com/benatti/evatesttask/model/dto/ProductRequestDto.java
@@ -0,0 +1,10 @@
+package com.benatti.evatesttask.model.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductRequestDto {
+    private String name;
+    private String description;
+
+}
---
+++ src/main/java/com/benatti/evatesttask/model/dto/ProductResponseDto.java
@@ -0,0 +1,11 @@
+package com.benatti.evatesttask.model.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductResponseDto {
+    private Long id;
+    private String name;
+    private String description;
+
+}

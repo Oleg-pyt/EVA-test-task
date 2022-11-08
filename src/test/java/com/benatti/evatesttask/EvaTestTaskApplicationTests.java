80520b92413a2e8f82be31931f50868b7a43ea2f
Oleh Sunytsia <ssunica0@gmail.com>
2022-11-08 11:23:17 +0200

    spring boot

---
+++ src/test/java/com/benatti/evatesttask/EvaTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benatti.evatesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
